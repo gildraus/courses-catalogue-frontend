89f85b6adfeb9d218ec976952271d1966ff51106
gildrausNV <nikola.vujcic.001@gmail.com>
2023-12-07 15:50:48 +0100

    Fix details page for small devices

--- src/components/main_page/CourseDetails.js
+++ src/components/main_page/CourseDetails.js
@@ -4,8 +4,125 @@ const CourseDetails = ({ selectedCourse, setSelectedCourse }) => {
   const course = selectedCourse;
   return (
     <div className="course-details-background">
+        
+        <div className="row">
+          <div className="course-details-header">
+            <h1>{course.name}</h1>
+            {course.departments}
+          </div>
+        </div>
 
-        <div className="course-details">
+        <div className="row">
+          <div className="course-details-video-box  col-lg-8 col-sm-12">
+            {course.video ? (
+              <div
+                className="iframe-div"
+                style={{
+                  position: "relative",
+                  paddingBottom: "56.25%",
+                  height: 0,
+                  overflow: "hidden",
+                }}
+              >
+                <iframe
+                  style={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    width: "100%",
+                    height: "100%",
+                  }}
+                  src={`https://www.youtube.com/embed/${course.video}`}
+                  title="Faculty of Organizational Sciences - promo video"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  allowFullScreen
+                ></iframe>
+              </div>
+            ) : (
+              <p>[катедра нема промотивни видео]</p>
+            )}
+          </div>
+
+          <div className="course-details-sidecard  col-lg-4 col-sm-12">
+            <h2>Картон предмета</h2>
+            <p>
+              Веб сајт: <a href={course.link}>{course.link}</a>
+            </p>
+            <p>Број ЕСПБ поена: {course.espb}</p>
+            <p>
+              Смер:{" "}
+              {course.studies.map((studie, index) => (
+                <p key={index}>{studie}</p>
+              ))}
+            </p>
+            <p>Семестар: {course.semester}</p>
+            <p>Статус: {course.status}</p>
+            <h2>Ограничења</h2>
+            <p><img src="./images/danger.png"/>{course.note}</p>
+            <p>
+              {course.restrictions.map((restriction, index) => (
+                <p key={index}><img src="./images/warning.png"/>{restriction}</p>
+              ))}
+            </p>
+          </div>
+
+          <div className="course-details-basic-info-box col-lg-8 col-sm-12">
+            <h3>Предавачи:</h3>
+            {course.lecturers.map((lecturer, index) => (
+              <p key={index}>{lecturer}</p>
+            ))}
+            <h3>Настава:</h3>
+            <p>{course.type_of_lecture}</p>
+
+            <h3>Начин полагања испита:</h3>
+            <p>{course.type_of_exam}</p>
+
+            <h3>Предуслов:</h3>
+            {course.preconditions.map((precondition, index) => (
+              <p key={index}>{precondition}</p>
+            ))}
+
+            <h3>Периодичност:</h3>
+            <p>{course.periodicity}</p>
+
+            <h3>Термини предавања:</h3>
+            {course.lecture_session_time.map((time, index) => (
+              <p key={index}>{time}</p>
+            ))}
+
+            <h3>Термини вежби:</h3>
+            {course.exercise_session_time.map((time, index) => (
+              <p key={index}>{time}</p>
+            ))}
+          </div>
+
+          <div className="course-details-detailed-info-box col-lg-8 col-sm-12">
+            <h2>Опширније информације</h2>
+            <br />
+            <h3 className="green-paragraph">Сажетак</h3>
+            {course.abstract}
+            <hr />
+            <h3 className="green-paragraph">Циљ</h3>
+            {course.objective}
+          </div>
+
+          <div className="course-details-literature col-lg-8 col-sm-12">
+            <h2>Садржај и литература</h2>
+            <br />
+            <h3 className="green-paragraph">Садржај</h3>
+            {course.content}
+            <hr />
+            <h3 className="green-paragraph">Литература</h3>
+            {course.literature.map((item, index) => (
+              <p key={index}>
+                {index + 1}. {item}
+              </p>
+            ))}
+          </div>
+        </div>
+
+        {/* <div className="course-details">
           <div className="course-details-header">
             <h1>{course.name}</h1>
             {course.departments}
@@ -129,7 +246,7 @@ const CourseDetails = ({ selectedCourse, setSelectedCourse }) => {
             </div>
           </div>
           
-        </div>
+        </div> */}
 
     </div>
 
